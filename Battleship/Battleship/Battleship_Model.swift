71556c5c463cf2f5968bf4f7424cc1e6cbdd6c5c
u0844210 <u0844210@1120-mac-12.lib.utah.edu>
2017-03-01 16:26:45 -0700

    Started battleship

---
+++ Battleship/Battleship/Battleship_Model.swift
@@ -0,0 +1,13 @@
+//
+//  Battleship_Model.swift
+//  Battleship
+//
+//  Created by u0844210 on 3/1/17.
+//  Copyright © 2017 cs4530. All rights reserved.
+//
+
+import UIKit
+
+class BatteshipModel {
+    
+}
