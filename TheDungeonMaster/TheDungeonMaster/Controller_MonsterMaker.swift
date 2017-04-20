af3838eb09848aba9ac147e0c9d1c140a8683b51
u0844210 <u0844210@kcmac-113.scl.utah.edu>
2017-04-19 23:45:45 -0600

    Lots of work with interface builder for monster maker

--- TheDungeonMaster/TheDungeonMaster/Controller_MonsterMaker.swift
+++ TheDungeonMaster/TheDungeonMaster/Controller_MonsterMaker.swift
@@ -11,4 +11,22 @@ import UIKit
 class MonsterMakerViewController: UIViewController {
     
     @IBOutlet weak var MonsterNameField: UITextField!
+    @IBOutlet weak var MonsterHP: UITextField!
+    @IBOutlet weak var MonsterAC: UITextField!
+    @IBOutlet weak var MonsterProficiency: UITextField!
+    
+    @IBOutlet weak var MonsterStr: UITextField!
+    @IBOutlet weak var MonsterDex: UITextField!
+    @IBOutlet weak var MonsterCon: UITextField!
+    @IBOutlet weak var MonsterInt: UITextField!
+    @IBOutlet weak var MonsterWis: UITextField!
+    @IBOutlet weak var MonsterChr: UITextField!
+    
+    @IBOutlet weak var MonsterStrSave: UISwitch!
+    @IBOutlet weak var MonsterDexSave: UISwitch!
+    @IBOutlet weak var MonsterConSave: UISwitch!
+    @IBOutlet weak var MonsterIntSave: UISwitch!
+    @IBOutlet weak var MonsterWisSave: UISwitch!
+    @IBOutlet weak var MonsterChrSave: UISwitch!
+    
 }
